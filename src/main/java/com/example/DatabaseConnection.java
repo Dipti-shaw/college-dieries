fdf66a027f2acafab1fdf60ff7377e5655dbbf52
G0dZilLAaA <kumawatmohit435@gmail.com>
2025-04-04 10:16:09 +0530

    done

--- src/main/java/com/example/DatabaseConnection.java
+++ src/main/java/com/example/DatabaseConnection.java
@@ -1,12 +1,14 @@
 package com.example;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
+
     static String url = "jdbc:mysql://localhost:3306/college_diaries";
     static String user = "mohit";
-    static String password = "mohit@123";
+    static String password = "Mohit@123";
 
     public static Connection getConnection() {
         try {
